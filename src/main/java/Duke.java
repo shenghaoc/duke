8a3138c2779bd16cbeab5a5d15f783ee90876e51
Chen Shenghao <>
2020-01-23 03:30:01 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,18 +4,32 @@ public class Duke {
 
     private static final String name = "Naruto";
     private static final Scanner sc = new Scanner(System.in);
+    private static final String line = "____________________________________________________________";
+    private static final String indent = "    ";
 
     public static void main(String[] args) {
         System.out.println("Hi! I'm " + name);
         System.out.println("How may I help you?");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         for (; ; ) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Naruto: See you later!");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("Naruto:");
+                System.out.println(indent + line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(indent + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(indent + line);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("Naruto: \"" + input + "\" added");
             }
-            System.out.println("Naruto: " + input);
         }
     }
 }
