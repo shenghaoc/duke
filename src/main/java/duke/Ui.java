69393e17f16f11a98c24ea09c64489942af60132
Chen Shenghao <>
2020-02-18 03:22:31 +0800

    Modify list and find methods to use lambdas and streams

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -3,6 +3,8 @@ package duke;
 import duke.task.Task;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Class to represent the user interface.
@@ -21,6 +23,7 @@ public class Ui {
 
     /**
      * Prints text from chat bot.
+     *
      * @param message Text to be said
      */
     protected String say(String message) {
@@ -29,7 +32,8 @@ public class Ui {
 
     /**
      * Adds duke.task to list and prints message to indicate success.
-     * @param message Information about added duke.task
+     *
+     * @param message    Information about added duke.task
      * @param taskNumber Number of tasks in list - 1
      */
     protected String taskAddedMessage(String message, int taskNumber) {
@@ -39,6 +43,7 @@ public class Ui {
 
     /**
      * Prints message between two horizontal bars.
+     *
      * @param message Message to be printed
      */
     protected String format(String message) {
@@ -54,6 +59,7 @@ public class Ui {
 
     /**
      * Prints the message with indentation of four spaces.
+     *
      * @param message Message to be printed
      */
     protected String indent(String message) {
@@ -62,14 +68,15 @@ public class Ui {
 
     /**
      * Prints list of tasks.
+     *
      * @param tasks List of tasks
      */
     protected String list(ArrayList<Task> tasks) {
         StringBuilder message = new StringBuilder(say("Here you go"));
         message.append(bar());
-        for (int i = 0; i < tasks.size(); i++) {
-            message.append(indent((i + 1) + ". " + tasks.get(i).toString())).append(System.lineSeparator());
-        }
+        IntStream
+                .range(0, tasks.size())
+                .forEachOrdered(i -> message.append(indent((i + 1) + ". " + tasks.get(i).toString())).append(System.lineSeparator()));
         return message.append(bar()).toString();
     }
 
@@ -77,11 +84,11 @@ public class Ui {
         StringBuilder message = new StringBuilder(say("Here are the matching tasks in your list"));
         message.append(bar());
         int defaultLength = message.length();
-        for (int i = 0; i < tasks.size(); i++) {
-            if (tasks.get(i).getDescription().contains(keyword)) {
-                message.append(indent((i + 1) + ". " + tasks.get(i).toString())).append(System.lineSeparator());
-            }
-        }
+        IntStream
+                .range(0, tasks.size())
+                .filter(i -> tasks.get(i).getDescription().contains(keyword))
+                .forEachOrdered(i -> message.append(indent((i + 1) + ". "
+                        + tasks.get(i).toString())).append(System.lineSeparator()));
         if (message.length() == defaultLength) {
             return partialMatchingItems(tasks, keyword);
         }
@@ -93,15 +100,15 @@ public class Ui {
                 say("No matching task found, here are the partially-matching tasks in your list"));
         message.append(bar());
         String[] subkeywords = keyword.split(" ");
-        for (String s : subkeywords) {
+        Arrays.stream(subkeywords).forEachOrdered(s -> {
             message.append(indent("Match \"" + s + "\"")).append(System.lineSeparator());
-            for (int i = 0; i < tasks.size(); i++) {
-                if (tasks.get(i).getDescription().contains(s)) {
-                    message.append(indent((i + 1) + ". " + tasks.get(i).toString())).append(System.lineSeparator());
-                }
-            }
+            IntStream
+                    .range(0, tasks.size())
+                    .filter(i -> tasks.get(i).getDescription().contains(s))
+                    .forEachOrdered(i -> message.append(indent((i + 1) + ". "
+                            + tasks.get(i).toString())).append(System.lineSeparator()));
             message.append(System.lineSeparator());
-        }
+        });
         return message.append(bar()).toString();
     }
 }
